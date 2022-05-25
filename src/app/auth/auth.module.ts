397533076ea238e779b700598e06f35e24c5cbe6
Asad Ali <asad.ali1010101@gmail.com>
2022-05-25 17:52:48 +0500

    fixes

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -9,6 +9,8 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login.component';
 import { CreateNewAccountComponent } from './create-new-account/create-new-account.component';
 import { SharedModule } from '@app/@shared';
+import { ForgotPasswordComponent } from './popup-component/forgot-password/forgot-password.component';
+import { ResetPasswordEmailComponent } from './popup-component/reset-password-email/reset-password-email.component';
 
 @NgModule({
   imports: [
@@ -20,6 +22,6 @@ import { SharedModule } from '@app/@shared';
     AuthRoutingModule,
     SharedModule,
   ],
-  declarations: [LoginComponent, CreateNewAccountComponent],
+  declarations: [LoginComponent, CreateNewAccountComponent, ForgotPasswordComponent, ResetPasswordEmailComponent],
 })
 export class AuthModule {}
