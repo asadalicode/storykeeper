a3457beb2703eac0ee659dc1bcc76c2f09a8d462
Asad Ali <asad.ali1010101@gmail.com>
2022-06-06 21:58:53 +0500

    fixes

--- src/app/@shared/components/footer/footer.component.ts
+++ src/app/@shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CredentialsService } from '@app/auth';
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-  constructor() {}
+  constructor(private credentials: CredentialsService) {}
 
   ngOnInit(): void {}
+
+  get isLoggedIn(): boolean {
+    return this.credentials.isAuthenticated();
+  }
+
+  routeTo(type: string) {
+    switch (type) {
+      case 'about':
+        window.open('https://www.storykeeperapp.com/', '_blank');
+        break;
+
+      case 'privacy':
+        window.open('https://www.storykeeperapp.com/privacy-policy', '_blank');
+        break;
+
+      case 'terms':
+        window.open('https://www.storykeeperapp.com/terms-of-service', '_blank');
+        break;
+
+      default:
+        break;
+    }
+  }
 }
